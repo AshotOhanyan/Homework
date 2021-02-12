5826f8aaa3685d603e4ab03b198744b502d28c75
ashot.ohanyan1.y <ashot.ohanyan1.y@gmail.com>
2021-02-12 21:11:36 +0400

    Add Func

--- Homework4/Homework4/Program.cs
+++ Homework4/Homework4/Program.cs
@@ -8,6 +8,12 @@ namespace Homework4
     {
         static void Main(string[] args)
         {
+            
+            
+            Console.WriteLine(Func());
+        }
+        public static string Func()
+        {
             string text = Console.ReadLine();
             string[] characters = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
             StringBuilder sb = new StringBuilder();
@@ -15,19 +21,28 @@ namespace Homework4
 
             for (int i = 0; i < charLength - 1; i++)
             {
-                if(characters[i].Length > 1)
+                if(characters[i].Length == 1 && characters[i].EndsWith('.'))
+                {
+                    continue;
+                }
+                if (characters[i].Length > 1)
                 {
                     sb = sb.Append(characters[i].Substring(1));
-                }       
+                }
                 sb = sb.Append(Char.ToLower(characters[i][0]));
-                sb = sb.Append("ey ");                
+                sb = sb.Append("ey ");
             }
 
             string lastSymbol = "";
             string last = characters[charLength - 1];
+            if(last.Length == 1 && last[0] == '.')
+            {
+                sb.Append('.');
+                return sb.ToString();
+            }
             if (last.EndsWith('.') || last.EndsWith('?'))
             {
-                
+
                 lastSymbol = last[last.Length - 1] + "";
                 last = last.Substring(0, last.Length - 1);
             }
@@ -37,8 +52,7 @@ namespace Homework4
                 sb = sb.Append(last.Substring(1));
             }
             sb.Append(Char.ToLower(last[0])).Append("ey").Append(lastSymbol);
-            
-            Console.WriteLine(sb.ToString());
+            return sb.ToString();
         }
     }
 }
